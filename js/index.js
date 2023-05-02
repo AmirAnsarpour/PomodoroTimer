93df00243f39b0ef0c62fb6142c6c2b9d4638019
AmirAnsarpour <amir.ansarpour2003@gmail.com>
2023-05-02 21:09:07 +0430

    full screen add

--- js/index.js
+++ js/index.js
@@ -103,3 +103,13 @@ resetBtn.addEventListener("click", function () {
     shortBtn.classList.remove("clicked");
     longBtn.classList.remove("clicked");
 });
+
+function toggleFullScreen() {
+    if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen();
+    } else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        }
+    }
+}
